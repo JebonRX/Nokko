8efb694afccf854dd9690fa2b5242b71c993a9b4
JebonRX <153726581+JebonRX@users.noreply.github.com>
2025-06-29 17:28:28 +0800

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -42,7 +42,7 @@ read -p "        Please Input Number  [1-14 or x] :  "  sys
 echo -e ""
 case $sys in
 1)
-add-host
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill;add-host
 ;;
 2)
 certv2ray
